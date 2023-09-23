b65e2b58cec17b6c8e39015d72b4a9722774cb27
Brettly <brettcd@icloud.com>
2023-09-23 05:35:18 -0700

    Update gametime calc function to account for the format of the period change from 1Q to Q1 mid-year.

--- config/functions.py
+++ config/functions.py
@@ -1,7 +1,13 @@
-def pandas_game_time_calc(row):
+#create a custom function to apply
+def game_time_calc(row):
     """function for pandas dataframe to calculate how much time has passed in the total game using the score time and the period elapsed time"""
     if row['score_period'] == 'OT':
-        return (15 * 4 * 60) + row['period_elapsed_time'] #if the score is in OT add the elapsed time in OT to a full 60 minutes of clock time
+        return (15 * 4 * 60) + row['period_elapsed_time']
     else:
-        first_digit = int(str(row['score_period'])[0])
-        return ((first_digit - 1) * 15 * 60) + row['period_elapsed_time'] #if the score isn't in OT add the elapsed time in the current quarter to the clock time completed in prior quarters
+        #try to grab quarter from both 'Q1' and '1Q' format - API changes mid year
+        try:
+            quarter = int(str(row['score_period'])[0])
+        except ValueError:
+            quarter = int(str(row['score_period'])[1])
+            
+        return ((quarter - 1) * 15 * 60) + row['period_elapsed_time']
