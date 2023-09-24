99606ea6bce89e1aa2fc21672478f0f79c9821e1
Brettly <brettcd@icloud.com>
2023-09-24 11:12:24 -0700

    Create new mappings file in the config directory to make our code more organized.

---
+++ config/mappings.py
@@ -0,0 +1,61 @@
+###FILE TO SAVE MAPPINGS###
+
+#period mapping to deal with inconsistent usage of the quarter/period names
+period_mapping = {
+    '1Q': 'Q1',
+    '2Q': 'Q2',
+    '3Q': 'Q3',
+    '4Q': 'Q4',
+    'OT': 'OT'
+}
+
+#summary data type mappings
+summary_dtype_mapping = {
+    'game_id': 'object',
+    'game_type': 'object',
+    'game_date_id': 'object',
+    'game_location': 'object',
+    'away_team_id': 'object',
+    'away_team': 'object',
+    'away_q1_score': 'int64',
+    'away_q2_score': 'int64',
+    'away_q3_score': 'int64',
+    'away_q4_score': 'int64',
+    'away_ot_score': 'int64',
+    'away_total_score': 'int64',
+    'away_total_plays': 'int64',
+    'away_total_yards': 'int64',
+    'away_passing_yards': 'int64',
+    'away_rushing_yards': 'int64',
+    'away_turnovers': 'int64',
+    'away_time_of_possession': 'object',  # Update to datetime after mapping
+    'away_result': 'object',
+    'home_team_id': 'object',
+    'home_team': 'object',
+    'home_q1_score': 'int64',
+    'home_q2_score': 'int64',
+    'home_q3_score': 'int64',
+    'home_q4_score': 'int64',
+    'home_ot_score': 'int64',
+    'home_total_score': 'int64',
+    'home_total_plays': 'int64',
+    'home_total_yards': 'int64',
+    'home_passing_yards': 'int64',
+    'home_rushing_yards': 'int64',
+    'home_turnovers': 'int64',
+    'home_time_of_possession': 'object',  # Update to datetime after mapping
+    'away_result': 'object',
+    'home_result': 'object',
+}
+
+#scoring detail data type mappings
+scoring_dtype_mapping = {
+    'team_id': 'object',
+    'score_type': 'category',  # Make score_type categorical
+    'score_period': 'object',
+    'score_time': 'object',  # convert to datetime in nextsteyp
+    'drive_detail': 'object',
+    'score_detail': 'object',
+    'away_team_score': 'int64',
+    'home_team_score': 'int64',
+}
