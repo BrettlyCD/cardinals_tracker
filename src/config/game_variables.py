475bf0846f5ce9704913e35fe3758af8b58a2ebb
Brettly <brettcd@icloud.com>
2023-09-27 16:57:20 -0700

    Move config directory underneath src directory.

---
+++ src/config/game_variables.py
@@ -0,0 +1,21 @@
+#ids of all Cardies games in 2022
+
+game_list_22 = [
+    # '20220911_KC@ARI',
+    # '20220918_ARI@LV',
+    # '20220925_LAR@ARI',
+    # '20221002_ARI@CAR',
+    # '20221009_PHI@ARI',
+    # '20221016_ARI@SEA',
+    # '20221020_NO@ARI',
+    # '20221030_ARI@MIN',
+    # '20221106_SEA@ARI',
+    # '20221113_ARI@LAR',
+    # '20221121_SF@ARI',
+    # '20221127_LAC@ARI',
+    # '20221212_NE@ARI',
+    # '20221218_ARI@DEN',
+    # '20221225_TB@ARI',
+    '20230101_ARI@ATL',
+    '20230108_ARI@SF',
+]
