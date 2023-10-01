e3dbc68404507f484acd178096ce7236ac41884d
Brettly <brettcd@icloud.com>
2023-09-30 22:15:52 -0700

    Add imports to etl fim file, test converting of dict to df in notebook file.

--- src/dim_tables_etl.py
+++ src/dim_tables_etl.py
@@ -4,6 +4,6 @@ import os
 import requests
 
 from dotenv import load_dotenv
-from config.dim_tables_static import score_type_dict, sportsbook_dict, game_type_dict
+from config.dim_tables_static import score_type_dict, sportsbook_dict, game_type_dict #import static dimension data
 
 
