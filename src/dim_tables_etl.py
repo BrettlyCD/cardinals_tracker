d5d5d66a7f7519b29152bf1952f47d6c7bffc620
Brettly <brettcd@icloud.com>
2023-09-30 22:10:07 -0700

    Create dim_tables_etl file.

---
+++ src/dim_tables_etl.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json
+import os
+import requests
+
+from dotenv import load_dotenv
+from config.dim_tables_static import score_type_dict, sportsbook_dict, game_type_dict
+
+
