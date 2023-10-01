6049107236fd1e9e3de2164cc4a1849bbd39d583
Brettly <brettcd@icloud.com>
2023-09-30 22:05:09 -0700

    Create .py file to store static dimension table data - game type, score type, sportsbook.

---
+++ src/config/dim_tables_static.py
@@ -0,0 +1,28 @@
+### File to save static dimensions to load into ETL processes
+
+#score types
+score_type_dict = {
+    "TD": 1,
+    "SF": 2,
+    "FG": 3
+}
+
+#sportsbook names
+sportsbook_dict = {
+    "betmgm": 1,
+    "bet365": 2,
+    "fanduel": 3,
+    "wynnbet": 4,
+    "unibet": 5,
+    "pointsbet": 6,
+    "betrivers": 7,
+    "ceasars_sportsbook": 8,
+    "draftkings": 9,   
+}
+
+#game types
+game_type_dict = {
+    "Preseason": 1,
+    "Regular Season": 2,
+    "Postseason": 3
+}
