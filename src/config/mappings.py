baa0d49c185b1cb57c78d02f29554b6a160a0f7f
Brettly <brettcd@icloud.com>
2023-10-01 11:50:21 -0700

    Add static dim mapping and update scoring and boxscore type mapping to reflect new data structure.

--- src/config/mappings.py
+++ src/config/mappings.py
@@ -12,25 +12,12 @@ period_mapping = {
 #summary data type mappings
 summary_dtype_mapping = {
     'game_id': 'object',
-    'game_type': 'object',
-    'game_date_id': 'object',
-    'game_location': 'object',
-    'away_team_id': 'object',
-    'away_team': 'object',
-    'away_q1_score': 'int64',
-    'away_q2_score': 'int64',
-    'away_q3_score': 'int64',
-    'away_q4_score': 'int64',
-    'away_ot_score': 'int64',
-    'away_total_score': 'int64',
-    'away_total_plays': 'int64',
-    'away_total_yards': 'int64',
-    'away_passing_yards': 'int64',
-    'away_rushing_yards': 'int64',
-    'away_turnovers': 'int64',
-    'away_time_of_possession': 'object',  # Update to datetime after mapping
-    'away_result': 'object',
     'home_team_id': 'object',
+    'away_team_id': 'object',
+    # 'game_type': 'object',
+    # 'game_date_id': 'object',
+    # 'game_location': 'object',
+    # 'away_team': 'object',
     'home_team': 'object',
     'home_q1_score': 'int64',
     'home_q2_score': 'int64',
@@ -44,18 +31,38 @@ summary_dtype_mapping = {
     'home_rushing_yards': 'int64',
     'home_turnovers': 'int64',
     'home_time_of_possession': 'object',  # Update to datetime after mapping
-    'away_result': 'object',
-    'home_result': 'object',
+    'away_q1_score': 'int64',
+    'away_q2_score': 'int64',
+    'away_q3_score': 'int64',
+    'away_q4_score': 'int64',
+    'away_ot_score': 'int64',
+    'away_total_score': 'int64',
+    'away_total_plays': 'int64',
+    'away_total_yards': 'int64',
+    'away_passing_yards': 'int64',
+    'away_rushing_yards': 'int64',
+    'away_turnovers': 'int64',
+    'away_time_of_possession': 'object',  # Update to datetime after mapping
+    'home_team_win_flag': 'int'
+    # 'away_result': 'object',
+    # 'home_result': 'object',
 }
 
 #scoring detail data type mappings
 scoring_dtype_mapping = {
-    'team_id': 'object',
-    'score_type': 'category',  # Make score_type categorical
+    'game_id': 'object',
+    'team_id': 'int64',
+    'score_type_id': 'int64',  # Make score_type categorical
     'score_period': 'object',
-    'score_time': 'object',  # convert to datetime in nextsteyp
+    'score_time': 'object',  # convert to datetime in function
     'drive_detail': 'object',
     'score_detail': 'object',
     'away_team_score': 'int64',
-    'home_team_score': 'int64',
+    'home_team_score': 'int64'
+}
+
+#static dim table data type mappings - id, label
+static_dim_dtype_mapping = {
+    'ID': 'int64',
+    'label': 'object'
 }
